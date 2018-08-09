2cf3988539941037ed5d1073f58a3bbeeabb0613
Kathryn Wheeler <kiwheel@bu.edu>
2018-08-09 16:04:48 -0400

    corrected typo

--- spectraFits/R/createModel_exp.R
+++ spectraFits/R/createModel_exp.R
@@ -14,11 +14,12 @@ createModel.Exp <- function(data,index){
   # data$mean.b <- 0.02
   # data$p.b <- 1/(0.005**2)
   inits <- list()
+
   if(index=="PSRI" || index=="RGI"){
     data$min.a <- 0
     data$max.a <- 100
     for(i in 1:nchain){
-      inits[[i]] <- list(a=rnorm(1,0.34,0.005),b=rnorm(1,0.02,0.03),muL=rnorm(1,mean(data$y[1:10]),0.05),k=rnorm(1,285,10))
+      inits[[i]] <- list(a=rnorm(1,0.34,0.005),b=rnorm(1,0.02,0.03),c=rnorm(1,mean(data$y[1:10]),0.05))
     }
   }
   else{
