739bd6f123ba0dc6bd982cf7d09b81a0a4203068
Kathryn Wheeler <kiwheel@bu.edu>
2018-08-10 11:38:18 -0400

    updated

--- checkForInSituFits.R
+++ checkForInSituFits.R
@@ -1,13 +1,12 @@
 mdl <- "LR"
 trees <- c("BE1","BE2","BE3","BE4","BE5","PO1","PO2","PO3","PO4","PO5","BI1","BI2","BI3","BI4","BI5")
 indices <- c("chl","car","NDVI_H","PRI","NDRE","GNDVI","GM1","RVI1","RVI2","LIC","CTR","GM2","VGM","PSRI","mSR","mND","DD","RGI","RE","NDVI_M","SIPI")
-missingMdls <- character()
 
 for(tr in 1:length(trees)){
   for(i in 1:length(indices)){
     fileName <- paste(trees[tr],"_2016_",indices[i],"_",mdl,"_varBurn.RData",sep="")
     if(!file.exists(fileName)){
-      misingMdls <- c(missingMdls,paste(trees[tr],"_",indices[i],sep=""))
+      print(paste(trees[tr],"_",indices[i],sep=""))
     }
   }
 }
