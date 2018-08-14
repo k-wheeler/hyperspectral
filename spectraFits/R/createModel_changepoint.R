a8699d5ecae7f3686f94400a7e94b1032c7531b6
Kathryn Wheeler <kiwheel@bu.edu>
2018-08-14 18:51:39 -0400

    fixed error

--- spectraFits/R/createModel_changepoint.R
+++ spectraFits/R/createModel_changepoint.R
@@ -13,15 +13,15 @@ createModel.CP <- function(data,index){
   inits <- list()
 
   if(index=="PSRI" || index=="RGI"){
-    data$min.a <- 0
-    data$max.a <- 100
+    data$min.a <- -100
+    data$max.a <- 0
     for(i in 1:nchain){
       inits[[i]] <- list(a=rnorm(1,-0.02,0.003),b=rnorm(1,0.12,0.03),muL=rnorm(1,mean(data$y[1:10]),0.05),k=rnorm(1,285,10))
     }
   }
   else{
-    data$min.a <- -100
-    data$max.a <- 0
+    data$min.a <- 0
+    data$max.a <- 100
     for(i in 1:nchain){
       inits[[i]] <- list(a=rnorm(1,0.02,0.003),b=rnorm(1,0.12,0.03),muL=rnorm(1,mean(data$y[1:10]),0.05),k=rnorm(1,285,10))
     }
