faf1dd5e4cece0b4702c1fb1b7233d7855f5e4f2
Kathryn Wheeler <kiwheel@bu.edu>
2018-08-22 18:36:26 -0400

    changing graphing

--- makeInsituFigure.R
+++ makeInsituFigure.R
@@ -142,8 +142,8 @@ for(i in 1:length(indices)){
       
       plot(dat$x,dat$y,pch=20,ylab="",xlab="",cex.axis=2)
       ciEnvelope(xseq,pi.Exp[1,],pi.Exp[3,],col=rgb(0,0,1,0.5))
-      ciEnvelope(xseq,ci.Exp[1,],ci.Exp[3,],col=rgb(0,0,1,0.2))
       ciEnvelope(xseq,pi.LR[1,],pi.LR[3,],col=rgb(1,0,0,0.5))
+      ciEnvelope(xseq,ci.Exp[1,],ci.Exp[3,],col=rgb(0,0,1,0.2))
       ciEnvelope(xseq,ci.LR[1,],ci.LR[3,],col=rgb(1,0,0,0.2))
       
       points(dat$x,dat$y,pch=20)
