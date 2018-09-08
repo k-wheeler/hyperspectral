21258928c8ea914a62274e815f4baf30654311e4
Kathryn Wheeler <kiwheel@bu.edu>
2018-09-08 17:41:40 -0400

    Decreased indices

--- makeInsituFigure.R
+++ makeInsituFigure.R
@@ -20,12 +20,13 @@ phenoLR <- function(beta0,beta1,xseq){
 phenoExp <- function(c,a,xseq,b){
   return(a * exp(b*(xseq-xseq[1])) + c)
 }
-indices <- c("NDRE","PRI","chl","NDVI_H","PSRI","car","mND","GNDVI")
+#indices <- c("NDRE","PRI","chl","NDVI_H","PSRI","car","mND","GNDVI")
+indices <- c("NDRE","PRI","chl")
 trees <- c("BE1","BI1","PO1")
 types <- c("LR","Exp")
 yr <- "2016"
 pdf("InSitu_Figure.pdf",width=20,height=20)
-par(mfrow=c(8,3), mai = c(0.25, 0.4, 0.15, 0.2))
+par(mfrow=c(3,3), mai = c(0.25, 0.4, 0.15, 0.2))
 
 for(i in 1:length(indices)){
   print(indices[i])
